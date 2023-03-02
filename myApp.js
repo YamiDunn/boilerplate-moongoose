18ba55dd014fab8a9c436d9c1bab053c16e91f14
YamiDunn <j.felipe.rodriguez@hotmail.com>
2023-03-01 20:15:54 -0500

    Working on error

--- myApp.js
+++ myApp.js
@@ -1,14 +1,10 @@
 //const mySecret = process.env['MONGO_URI'];
 require('dotenv').config();
 let express = require('express');
-let app = express();
-console.log("Hello World");
 const mongoose = require('mongoose');
 let validator = require('validator');
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-
-//let Person;
 
 const Schema = mongoose.Schema;
 
